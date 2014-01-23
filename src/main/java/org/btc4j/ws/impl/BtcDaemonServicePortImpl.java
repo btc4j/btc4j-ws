c3d3d1641ed9c2f69a5775ff39e3b69dfd5a758c
ggbusto <email@ggonzalez.info>
2014-01-23 14:20:56 -0600

    eod commit

---
+++ src/main/java/org/btc4j/ws/impl/BtcDaemonServicePortImpl.java
@@ -0,0 +1,54 @@
+/*
+ The MIT License (MIT)
+ 
+ Copyright (c) 2013, 2014 by Guillermo Gonzalez, btc4j.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+package org.btc4j.ws.impl;
+
+import java.util.logging.Logger;
+
+import org.btc4j.ws.BtcDaemonServicePort;
+import org.btc4j.ws.BtcException;
+import org.btc4j.ws.HelpReq;
+import org.btc4j.ws.HelpResp;
+
+@javax.jws.WebService(
+                      serviceName = "BtcDaemonService",
+                      portName = "BtcDaemonServicePort",
+                      targetNamespace = "http://www.btc4j.org/ws/",
+                      endpointInterface = "org.btc4j.ws.BtcDaemonServicePort")
+                     
+public class BtcDaemonServicePortImpl implements BtcDaemonServicePort {
+    private static final Logger LOG = Logger.getLogger(BtcDaemonServicePortImpl.class.getName());
+
+    public HelpResp help(HelpReq params) throws BtcException    { 
+        LOG.info("Executing operation help");
+        System.out.println(params);
+        try {
+            HelpResp _return = null;
+            return _return;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+    }
+}
